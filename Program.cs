80393aa32ecfa6bb057d8fa1cd0005e224852a46
mooooms <its.mammy0@gmail.com>
2023-04-23 13:13:34 +0300

    created operations

--- Program.cs
+++ Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Введите первое число");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите операцию");
+            string read = Console.ReadLine();
+            Console.WriteLine("Введите второе число");
+            int b=Convert.ToInt32(Console.ReadLine());
+
+            switch (read)
+            {
+                case "+":
+                    Console.WriteLine(a + b);
+                    break;
+                case "-":
+                    Console.WriteLine(a - b);
+                    break;
+                case "*":
+                    Console.WriteLine(a * b);
+                    break;
+                case "/":
+                    Console.WriteLine(a / b);
+                    break;
+
+                default: Console.WriteLine("такой операции нет");
+                    break;
+            }
+
         }
+
     }
 }
